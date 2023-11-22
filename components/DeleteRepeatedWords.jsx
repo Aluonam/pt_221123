bcabd754c96c570ad472f2b8036fa05c81c1b66c
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 11:55:53 +0100

    feat: functionality with new set inside array and ...
    const handleDetected = ()=>{
        const words = userSentence.split(" ");
        const detected = [...new Set(words)];
        const sentence = detected.join(' ')
    
        setNewSentence(sentence)
      }

--- components/DeleteRepeatedWords.jsx
+++ components/DeleteRepeatedWords.jsx
@@ -4,10 +4,13 @@ const DeleteRepeatedWords = () => {
 
   const [userSentence, setUserSentence] = useState();
 
+  const [newSentence, setNewSentence] = useState()
+
   const handleDetected = ()=>{
-    const copyArr = structuredClone(userSentence.split(" "))
-    const detected = new Set(copyArr)
-    console.log(detected)
+    const words = userSentence.split(" ");
+    const detected = [...new Set(words)];
+    const sentence = detected.join(' ')
+    setNewSentence(sentence)
 
   }
 
@@ -15,6 +18,7 @@ const DeleteRepeatedWords = () => {
     <>
     <input placeholder='Write a sentence' onChange={(e)=>{setUserSentence(e.target.value)}}></input>
     <button onClick={()=>{handleDetected()}}>Delete repeated words</button>
+    {newSentence}
     </>
   )
 }
