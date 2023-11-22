4e5c4a56c866982619b6b86e9d36d5ce2f9eb4e0
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 13:40:11 +0100

    feat: basic skeleton

--- components/APIconsume.jsx
+++ components/APIconsume.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const APIconsume = () => {
+
+
+    const [dataAPI, setDataAPI] = useState([])
+    const [dataUser, setDataUser] = useState()
+
+
+    
   return (
-    <div>APIconsume</div>
+    <div>APIconsume
+        <input onChange={()=>{}}></input>
+        <button onClick={()=>{apiCall()}}>Search</button>
+    </div>
   )
 }
 
