06b6a132d7917971dbb5dc3ace05aa5507757176
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 10:59:51 +0100

    input user and input password functionality

--- components/FormPassword.jsx
+++ components/FormPassword.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormPassword = () => {
+
+    const [userName, setUserName] = useState()
+    const [userPassword, setUserPassword] = useState()
   return (
     <>
     <h3>User</h3>
-    <input type='text' placeholder='User name'></input>
+    <input type='text' placeholder='User name' onChange={(e)=>{setUserName(e.target.value)}}></input>
     <h3>Password</h3>
-    <input type='password' placeholder='User password'></input>
+    <input type='password' placeholder='User password' onChange={(e)=>{setUserPassword(e.target.value)}}></input>
     </>
   )
 }
