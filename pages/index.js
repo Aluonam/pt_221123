74104d2738ad0d2e4121ffe75ee23eedaadadcdd
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 11:57:22 +0100

    feat: component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import FormPassword from '@/components/FormPassword'
 import DeleteRepeatedWords from '@/components/DeleteRepeatedWords'
+import Counter from '@/components/Counter'
 
 export default function Home() {
   return (
@@ -17,6 +18,9 @@ export default function Home() {
        <br></br>
        <br></br>
        <DeleteRepeatedWords></DeleteRepeatedWords>
+       <br></br>
+       <br></br>
+       <Counter></Counter>
       </main>
     </>
   )
