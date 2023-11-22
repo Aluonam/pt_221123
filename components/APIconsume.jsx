9aa5e4b62e8ca864b73fe12e4a230dc7549b294b
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 13:48:05 +0100

    feat: number props

--- components/APIconsume.jsx
+++ components/APIconsume.jsx
@@ -10,15 +10,30 @@ const APIconsume = () => {
         try{ 
           const fetchUrl = await fetch(`https://randomuser.me/api/?results=${num}`)
           const data = await fetchUrl.json()
-          console.log(data.results)
+          setDataAPI(data.results)
   
         }catch(error){error,"error detected"}
       }
     
+      const tableUserData = dataAPI.map((person)=>{
+        return(
+            <>
+            <ul>
+                <li>
+                   {person.name.first} {person.name.last}
+                </li>
+            </ul>
+            
+            </>
+        )
+      })
   return (
     <div>
+        <h3>API CALL</h3>
+        <h5>Write a number for view person</h5>
         <input onChange={(e)=>{setDataUser(e.target.value)}} type='number'></input>
         <button onClick={()=>{apiCall(dataUser)}}>Search</button>
+        {tableUserData}
     </div>
   )
 }
