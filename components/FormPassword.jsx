b2139d1bdc39ddd13820afcb845d280c50494e39
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 10:58:11 +0100

    initial Form non-functional

---
+++ components/FormPassword.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FormPassword = () => {
+  return (
+    <>
+    <h3>User</h3>
+    <input type='text' placeholder='User name'></input>
+    <h3>Password</h3>
+    <input type='password' placeholder='User password'></input>
+    </>
+  )
+}
+
+export default FormPassword
