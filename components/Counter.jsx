248b53a3b316403c42b8e220612f5d1f351eee38
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 12:01:04 +0100

    feat: counter functionality

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+
+    const [counter, setCounter] = useState(0)
   return (
     <div>
         <h3>Counter</h3>
-        <button onClick={()=>{}}>+</button>
-        <button onClick={()=>{}}>-</button>
+        {counter}
+        <br/>
+        <button onClick={()=>{setCounter(counter + 1)}}>.   +   .</button>
+        <button onClick={()=>{setCounter(counter - 1)}}>.   -   .</button>
     </div>
   )
 }
