2da2c7e542be952f1e4bea9b7e8bd0d9d8f63509
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 13:41:23 +0100

    feat: data consumption

--- components/APIconsume.jsx
+++ components/APIconsume.jsx
@@ -6,7 +6,14 @@ const APIconsume = () => {
     const [dataAPI, setDataAPI] = useState([])
     const [dataUser, setDataUser] = useState()
 
-
+    const apiCall = async () => {
+        try{ 
+          const fetchUrl = await fetch(`https://randomuser.me/api/?results=5`)
+          const data = await fetchUrl.json()
+          console.log(data.results)
+  
+        }catch(error){error,"error detected"}
+      }
     
   return (
     <div>APIconsume
