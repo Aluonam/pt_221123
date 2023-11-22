7ce7ed3ed260bc3ec00c2f145396415276538ea6
Paula Cano <paulacanoroman@gmail.com>
2023-11-22 11:09:38 +0100

    feat: component

---
+++ components/DeleteRepeatedWords.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteRepeatedWords = () => {
+  return (
+    <>
+    <input placeholder='Write a sentence'></input>
+    </>
+  )
+}
+
+export default DeleteRepeatedWords
